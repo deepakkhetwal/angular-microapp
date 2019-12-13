d1a8e41f40da52011c9dc82cd8ac92b67bfc1ffa
shahmonika <shaah.ms@gmail.com>
2019-12-13 08:45:06 -0500

    push changes

---
+++ projects/case-notes/src/app/page1/page1.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `
+  <div class="card">
+    <div class="header">
+      <h1>Case Notes</h1>
+    </div>
+    <div class="content">
+    <button class="btn btn-primary">Create New </button>
+  <table class="table table-contensed table-striped">
+      <thead>
+      <tr>
+          <th>Created By</th>
+          <th>Note Date</th>
+          <th>Case Note Title</th>
+          <th>Service Names</th>
+          <th> Actions </th>
+      </tr>
+      </thead>
+      <tr>
+          <td>Christopher John</td>
+          <td>12/12/2019</td>
+          <td>case pending</td>
+          <td>Employee Turn Over Analysis</td>
+          <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Christopher John</td>
+        <td>12/12/2019</td>
+        <td>case pending</td>
+        <td>Employee Turn Over Analysis</td>
+        <td></td>
+      </tr>
+
+      </table>
+  </div>
+  `
+})
+export class Page1Component  {
+}
